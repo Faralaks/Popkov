b4da8f0edd7eb40a6781f435d11153528e96fa69
LumiMura <LumiMura@yandex.ru>
2020-03-04 18:00:16 +0300

    Задание 4

--- Практика 2/Script4.js
+++ Практика 2/Script4.js
@@ -1,13 +1,13 @@
 function answers() {
     count = 0;
-    if (!document.getElementById("r11").checked) count +=1;
-    if (!document.getElementById("r22").checked) count +=1;
-    if (!document.getElementById("r31").checked) count +=1;
-    if (count === 0){
+    if (document.getElementById("r11").checked) count +=1;
+    if (document.getElementById("r22").checked) count +=1;
+    if (document.getElementById("r31").checked) count +=1;
+    if (count === 3){
         document.getElementById("art").hidden = false  }
     else {
-        document.getElementById("res").value = `Неправильных ответов: ${count}  `}
-    if (count === 0){
+        document.getElementById("res").value = `Неправильных ответов: ${3 - count}  `}
+    if (count === 3){
         document.getElementById("res").hidden = true}
 }
 function changes() {
