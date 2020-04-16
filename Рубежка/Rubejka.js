dbddbe9746220a64426d7d489fe2ae6c16aa3111
Faralaks <Faralaks@ya.ru>
2020-04-16 19:57:56 +0300

    Начал выполнять рубежный контроль. JS - самый отвратный язык, на которо я когда-либо программировал.

--- Рубежка/Rubejka.js
+++ Рубежка/Rubejka.js
@@ -1,10 +1,7 @@
 let  saved = [3, 2]; // Тут будут сохраняться вводимые размеры таблицы, чтобы можно было восстановить предыдущее значение
-let status = "red"; //  В последствие влияет на цвет первого квадратика
-
-function serColor(cell) {
-    cell.style.background = status;
-
-}
+let savedColor = [201, 0, 0]; //  В последствие влияет на цвет первого квадратика
+let colors = [];
+let counts = [];
 
 function regenTable() { // Функция для создания таблицы
     let table = $("#myTable"); // div, в который будет добавлена таблица
@@ -14,7 +11,7 @@ function regenTable() { // Функция для создания таблицы
     let sizeY = yField.val(); //  Длина таблицы
     let x, y;
     let newRow;
-    let newCol;
+    let newCell;
 
     if (isNaN(sizeX) || isNaN(parseInt(sizeX))) {x = saved[0]; xField.val(x)}
     else if (sizeX === "0") {x = 1; xField.val(1)}
@@ -29,13 +26,12 @@ function regenTable() { // Функция для создания таблицы
     for (let i = 0; i < parseInt(y); i++) {
         newRow = document.createElement("tr");
         for (let j = 0; j < parseInt(x); j++) {
-            newCol = document.createElement("td");
-            newCol.textContent = i.toString()+"_"+j.toString();
-            newCol .id = "col"+i.toString()+"_"+j.toString();
-            newCol.onclick = function () {
-                serColor(this)
+            newCell = document.createElement("td");
+            newCell .id = "col"+i.toString()+"_"+j.toString();
+            newCell.onclick = function () {
+                this.style.background = `rgb(${savedColor[0]}, ${savedColor[1]}, ${savedColor[2]})`;
             };
-            newRow.append(newCol);
+            newRow.append(newCell);
         }
         table.append(newRow);
         saved = [x, y] // сохраняем на использованые значения
@@ -43,14 +39,109 @@ function regenTable() { // Функция для создания таблицы
 }
 
 function changeColor() {
-    if (status === "red") {
-        status = "blue";
-        $("#color1").css("backgroundColor", "blue");
+    let redOrBlue = $("#color1");
+    if (redOrBlue.css("backgroundColor") === "rgb(255, 0, 0)") {
+        savedColor = [0, 0, 255];
+        redOrBlue.css("backgroundColor","rgb(0, 0, 255)");
     }
-    else {
-        status = "red";
+    else if (redOrBlue.css("backgroundColor") === "rgb(0, 0, 255)") {
+        savedColor = [255, 0, 0];
+        redOrBlue.css("backgroundColor", "rgb(255, 0, 0)");
+    }
+    console.log(savedColor.join(" "));
+}
 
-        $("#color1").css("backgroundColor", "red");
+function showError(errorLine, errorMsg) {
+    errorLine.text(errorMsg);
+    errorLine.show();
+}
+
+function addColor() {
+    let colorsBlock = $("#colorsBlock");
+    let r = parseInt($("#colorR").val());
+    let g = parseInt($("#colorG").val());
+    let b = parseInt($("#colorB").val());
+    let colorCode = `${r}_${g}_${b}`;
+    let errorLine = $("#errors");
+
+    if (isNaN(r) || isNaN(g) || isNaN(b)) {
+        showError(errorLine,"Одно из полей имеет некорректное значение");
+        return;
     }
+    else if (r > 200 || g > 200 || b > 200) {
+        showError(errorLine,"Значение в полях не может привышать 200");
+        return;
+    }
+    else if (colors.indexOf(colorCode) !== -1) {
+        showError(errorLine,"Такой цвет уже существует на палитре");
+        return
+    }
+    else if (r+g+b === r*3) {
+        showError(errorLine, "Оттенки серого запрещены!");
+        return
+    }
+    else {errorLine.hide();}
+
+    let newColor = document.createElement("div");
+
+    newColor.onclick = function () {
+        savedColor = this.id.split("_");
+        counts[colors.indexOf(this.id)]++;
+    };
+    newColor.className = "colorSqr";
+    newColor.id = colorCode;
+    newColor.style.background = `rgb(${r}, ${g}, ${b})`;
+
+    colors.push(colorCode);
+    counts.push(0);
+    colorsBlock.append(newColor);
+}
+
+function delColor() {
+    let name = "#"+savedColor.join("_");
+    $(name).detach();
+    let colorIndex = colors.indexOf(savedColor)-1;
+    colors.splice(colorIndex, 1);
+    counts.splice(colorIndex, 1);
+    console.log(colors, counts);
 
 }
+
+
+function sortKey(a, b) {
+    if (counts[colors.indexOf(a)] > counts[colors.indexOf(b)]) {
+        return -1;
+    }
+    if (counts[colors.indexOf(a)] < counts[colors.indexOf(b)]) {
+        return 1;
+    }
+    else {return 0}
+}
+
+function sortColors() {
+    let newNumLine = colors.slice();
+    let colorsBlock = $("#colorsBlock");
+    newNumLine = newNumLine.sort(sortKey);
+    colorsBlock.empty();
+
+    newNumLine.forEach(function (item)  { // Этот код дублируется, потому что не хватило времени(
+        let newColor = document.createElement("div");
+        let itemS = item.split("_");
+        newColor.onclick = function () {
+            savedColor = this.id.split("_");
+            counts[colors.indexOf(this.id)]++;
+        };
+        newColor.className = "colorSqr";
+        newColor.id = item;
+        console.log(item);
+        newColor.style.background = `rgb(${itemS[0]}, ${itemS[1]}, ${itemS[2]})`;
+
+        colorsBlock.append(newColor);
+    });
+
+
+}
+
+window.onload = function() {
+    regenTable();
+};
