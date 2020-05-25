a3ee0bfb833ae9f4b1d4fc8f9e53e99f168563b0
Faralaks <Faralaks@ya.ru>
2020-05-26 02:28:43 +0300

    первое задание готово, а моя гнилая кнопка очень очень красивая. осталось только в 5 заданиее доделать расположение чекбокса

--- практика 5/task1.js
+++ практика 5/task1.js
@@ -16,3 +16,39 @@ function filltable() {
     }
     document.getElementById("TableToFill").innerHTML=s;
 }
+
+
+let rowCount = 3;
+let colCount = 3;
+
+function addRow() {
+    let table = document.getElementsByTagName("tbody")[0];
+    let newRow = document.createElement("tr");
+    let newCol;
+    for (let i = 0; i < colCount; i++) {
+        newCol = document.createElement("td");
+        newCol.innerHTML = '<td onclick="tdc(this)">&nbsp;</td>';
+        newCol.onclick = function () {
+            tdc(this)
+        };
+        newRow.append(newCol);
+    }
+    table.append(newRow);
+
+}
+
+function addCol() {
+    let elems = document.getElementsByTagName("tr");
+    let newCol;
+
+    for (let i = 0; i < elems.length; i++) {
+        newCol = document.createElement("td");
+        newCol.innerHTML = '<td onclick="tdc(this)">&nbsp;</td>';
+        newCol.onclick = function () {
+            tdc(this)
+        };
+        elems[i].append(newCol);
+    }
+    colCount++;
+}
+
